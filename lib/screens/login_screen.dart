7a95c8eb177d61d24ec7c2ad7daa877fd394daef
jainvasu1 <vasubajaj2001@gmail.com>
2026-06-11 15:57:02 +0530

    style:alignment of login box fixed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -16,132 +16,150 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: SafeArea(
         //safe area is used for avoid ui overlap with notch, status bar etc.
-        child: SingleChildScrollView(
-          child: Center(
-            //Centers your login card on screen
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Card(
-                elevation: 5, //elevation for shadow
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    children: [
-                      // Logo / Title
-                      const Text(
-                        "Welcome Back",
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 1.2,
-                        ),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              //used to make screen scrollable when keyboard opens
+              child: ConstrainedBox(
+                //this ensures full height so centering works
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: Center(
+                  //Centers your login card on screen
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Card(
+                      elevation: 5, //elevation for shadow
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      const SizedBox(height: 30),
-
-                      //FORM
-                      Form(
-                        key: _formKey, //connect form with _formkey
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
                         child: Column(
+                          mainAxisSize: MainAxisSize
+                              .min, //prevents column from stretching full height
                           children: [
-                            // Email Field
-                            TextFormField(
-                              decoration: const InputDecoration(
-                                labelText: "Email",
-                                border: OutlineInputBorder(),
+                            // Logo / Title
+                            const Text(
+                              "Welcome Back",
+                              style: TextStyle(
+                                fontSize: 32,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1.2,
                               ),
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  //cannot empty
-                                  return "Enter email";
-                                }
-                                if (!value.contains('@')) {
-                                  //must contain @
-                                  return "Enter valid email";
-                                }
-                                return null;
-                              },
                             ),
-                            const SizedBox(height: 20),
+                            const SizedBox(height: 30),
 
-                            // Password Field
-                            TextFormField(
-                              obscureText: true, //Hides password(***)
-                              decoration: const InputDecoration(
-                                labelText: "Password",
-                                border: OutlineInputBorder(),
-                              ),
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return "Enter password";
-                                }
-                                if (value.length < 6) {
-                                  return "Min 6 characters required";
-                                }
-                                return null;
-                              },
-                            ),
-                            const SizedBox(height: 15),
-
-                            //Forgot Password
-                            Align(
-                              alignment: Alignment.centerRight,
-                              child: TextButton(
-                                onPressed: () {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                      content: Text("Forgot Password clicked"),
+                            //FORM
+                            Form(
+                              key: _formKey, //connect form with _formkey
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  // Email Field
+                                  TextFormField(
+                                    decoration: const InputDecoration(
+                                      labelText: "Email",
+                                      border: OutlineInputBorder(),
                                     ),
-                                  );
-                                },
-                                child: const Text("Forgot Password?"),
-                              ),
-                            ),
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        //cannot empty
+                                        return "Enter email";
+                                      }
+                                      if (!value.contains('@')) {
+                                        //must contain @
+                                        return "Enter valid email";
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  const SizedBox(height: 20),
 
-                            const SizedBox(height: 10),
+                                  // Password Field
+                                  TextFormField(
+                                    obscureText: true, //Hides password(***)
+                                    decoration: const InputDecoration(
+                                      labelText: "Password",
+                                      border: OutlineInputBorder(),
+                                    ),
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return "Enter password";
+                                      }
+                                      if (value.length < 6) {
+                                        return "Min 6 characters required";
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  const SizedBox(height: 15),
 
-                            //Sign In Button
-                            SizedBox(
-                              width: double.infinity,
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  if (_formKey.currentState!.validate()) {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(
-                                        content: Text("Signing in..."),
-                                      ),
-                                    );
+                                  //Forgot Password
+                                  Align(
+                                    alignment: Alignment.centerRight,
+                                    child: TextButton(
+                                      onPressed: () {
+                                        ScaffoldMessenger.of(
+                                          context,
+                                        ).showSnackBar(
+                                          const SnackBar(
+                                            content: Text(
+                                              "Forgot Password clicked",
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                      child: const Text("Forgot Password?"),
+                                    ),
+                                  ),
 
-                                    Navigator.pushReplacementNamed(
-                                      context,
-                                      '/home',
-                                    ); //it uses for routing to home screen if you successfully login then it redirect to the login
-                                  }
-                                },
-                                child: const Text("Sign In"),
-                              ),
-                            ),
+                                  const SizedBox(height: 10),
 
-                            const SizedBox(height: 15),
+                                  //Sign In Button
+                                  SizedBox(
+                                    width: double.infinity,
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        if (_formKey.currentState!.validate()) {
+                                          ScaffoldMessenger.of(
+                                            context,
+                                          ).showSnackBar(
+                                            const SnackBar(
+                                              content: Text("Signing in..."),
+                                            ),
+                                          );
 
-                            // Bottom Sign Up
-                            TextButton(
-                              onPressed: () {},
-                              child: const Text(
-                                "Don't have an account? Sign up",
+                                          Navigator.pushReplacementNamed(
+                                            context,
+                                            '/home',
+                                          ); //it uses for routing to home screen if you successfully login then it redirect to the login
+                                        }
+                                      },
+                                      child: const Text("Sign In"),
+                                    ),
+                                  ),
+
+                                  const SizedBox(height: 15),
+
+                                  // Bottom Sign Up
+                                  TextButton(
+                                    onPressed: () {},
+                                    child: const Text(
+                                      "Don't have an account? Sign up",
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
                           ],
                         ),
                       ),
-                    ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ),
+            );
+          },
         ),
       ),
     );
